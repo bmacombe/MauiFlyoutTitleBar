7039b5afe80cfe86397e4e005471386cc925f353
Brian Macomber <brian@agspectrum.com>
2024-08-30 13:32:31 -0500

    Setup repro

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,6 +6,48 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
+		MainPage = new TestFlyoutPage();
+	}
+}
+
+public class TestFlyoutPage : FlyoutPage
+{
+	public TestFlyoutPage()
+	{
+		FlyoutLayoutBehavior = FlyoutLayoutBehavior.Popover;
+
+		Flyout = new ContentPage
+		{
+			Title = "Flyout",
+			BackgroundColor = Colors.Red,
+			Content = new StackLayout
+			{
+				Children = {
+					new Label { Text = "Flyout" }
+				}
+			}
+		};
+
+		var contentPage = new ContentPage
+		{
+			BackgroundColor = Colors.Green,
+			Title = "Detail",
+			Content = new StackLayout
+			{
+				Children = {
+					new Label { Text = "Detail" }
+				}
+			}
+		};
+
+		Detail = new NavigationPage(contentPage);
+		var button = new Button() { Text = "Menu" };
+		button.Clicked += (s, e) => IsPresented = true;
+		NavigationPage.SetTitleView(contentPage, button);
+	}
+
+	public override bool ShouldShowToolbarButton()
+	{
+		return false;
 	}
 }
